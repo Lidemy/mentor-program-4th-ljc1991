b2b142ad1338c1d0f7891a99b686ae3a86b217e7
ljc1991 <ljc19911551@gmail.com>
2020-06-28 18:43:32 +0800

    week2 HW done

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,22 @@
 function join(arr, concatStr) {
-  
+  let result = ''
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== arr.length - 1) {
+      result += arr[i]
+      result += concatStr
+    } else {
+      result += arr[i]
+    }
+  }
+  return result
 }
 
 function repeat(str, times) {
-  
+  let result = ''
+  for (let i = 0; i < times; i++) {
+    result += str
+  }
+  return result
 }
 
 console.log(join(['a'], '!'));
